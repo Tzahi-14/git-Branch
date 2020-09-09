93e29eac5d2ef6efa47cdfd5a4fa9d06edfed65a
tzahidavid014@gmail.com <your_email@gmool.mom>
2020-09-09 19:07:15 +0300

    new

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
+const port = 3010
+app.listen(port, function () {
+    console.log(`running server on port ${port}`)
+})
